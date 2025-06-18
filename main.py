4a35daf3f1719c6366fd1d006ca96fb60b8c46bb
bshalalom <benjamin.shalom2001@gmail.com>
2025-06-18 23:45:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from backend.routers import analyze_all
+from backend.routers import openai_agents
+from backend.routers import perplexity_agent
 
 app = FastAPI()
 
+# Binde alle Router ein
+app.include_router(analyze_all.router)
+app.include_router(openai_agents.router)
+app.include_router(perplexity_agent.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
